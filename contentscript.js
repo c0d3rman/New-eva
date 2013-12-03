fe729232b75ef57e0f727b7b205b1fb559044ddd
c0d3rman <yonilern@gmail.com>
2013-12-02 16:22:54 -0800

    Classes Hash
    Added hash with verified class title texts -> names. Also added
    educated guesses (not comprehensive)

--- contentscript.js
+++ contentscript.js
@@ -11,6 +11,37 @@ s.onload = function() {
 var parent = document.getElementById("__thermo").parentElement;
 parent.parentElement.removeChild(parent);
 
+//Create hash of title-text -> class name
+var titlehash = {
+	//verified
+	'For class "9th CESL".': 'CESL',
+	'For class "9th Comp Sci - Elegant Logic 2013-2014.1".': 'Advanced Programming',
+	'For class "9th DT Liquid 2013-2014.1".': 'Design Thinking',
+	'For class "9th English Baldwin 2013-2014.1".': 'English',
+	'For class "9th History Rousseau 2013-2014.1".': 'History',
+	'For class "9th Math Khayyam 2013-2014.1".': 'Math',
+	'For class "9th QUEST - All Grade 2013-2014.1".': 'Quest',
+	'For class "9th Quest 2 2013-2014.1".': 'Quest',
+	'For class "9th Robotics 2013-2014.1".': 'Robotics',
+	'For class "9th Science and Society 2013-2014.1".': 'Science and Society',
+	'For class "9th Science Liquid 2013-2014.1".': 'Science',
+	'For class "9th Science of Mind Liquid 2013-2014.1".': 'Science of Mind',
+	//educated guesses
+	'For class "9th DT Solid 2013-2014.1".': 'Design Thinking',
+	'For class "9th DT Gas 2013-2014.1".': 'Design Thinking',
+	'For class "9th DT Plasma 2013-2014.1".': 'Design Thinking',
+	'For class "9th Science Solid 2013-2014.1".': 'Science',
+	'For class "9th Science Gas 2013-2014.1".': 'Science',
+	'For class "9th Science Plasma 2013-2014.1".': 'Science',
+	'For class "9th Science of Mind Solid 2013-2014.1".': 'Science of Mind',
+	'For class "9th Science of Mind Gas 2013-2014.1".': 'Science of Mind',
+	'For class "9th Science of Mind Plasma 2013-2014.1".': 'Science of Mind',
+	'For class "9th English Locke 2013-2014.1".': 'English',
+	'For class "9th History Machiavelli 2013-2014.1".': 'History',
+	'For class "9th Math Turing 2013-2014.1".': 'Math',
+	'For class "9th Comp Sci - Computing in Everything 2013-2014.1".': 'Advanced Programming',
+};
+
 //Process all elements
 var all = document.getElementsByTagName("*");
 var max = all.length; //save length to prevent NodeList funkiness
