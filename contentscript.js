2b5b3cff17bd383057b80b569f7a0e7991128308
c0d3rman <yonilern@gmail.com>
2013-12-04 10:22:50 -0800

    Image Restructuring & More
    -Restructured If for optimization.
    -Made "The Nueva School" regex search ALL elements.
    -Added image to "Today"
    -Changed images to local
    -Changed the way images are loaded to allow for easy image loading in
    the future

--- contentscript.js
+++ contentscript.js
@@ -54,32 +54,29 @@ var titlehash = {
 var all = document.getElementsByTagName("*");
 var max = all.length; //save length to prevent NodeList funkiness
 for (var i = max-1; i > 0; i--) {
+	//Kill browser warning, .hw_post, and .hw_remind
+	if (all[i].className == 'hw_post' || all[i].className == 'hw_remind' || all[i].className == 'errorMessage') {
+		all[i].parentNode.removeChild(all[i]);
+	}
 	//Handle homework assignments
-	if ((all[i].className == 'hw_due') ) {
+	else if (all[i].className == 'hw_due') {
 		all[i].removeChild(all[i].firstChild); //Kill "due today"s
 		var title = titlehash[all[i].firstChild.title]; //Save title-text for reference (firstChild problems), hash lookup for performance
 		all[i].style.listStyleType = 'none'; //Remove bullet points
 		all[i].firstChild.title = title; //Set title-text to classname 
 		all[i].innerHTML = title + ": " + all[i].innerHTML; //Add classname
+	} else if (all[i].className == "calendarNoSchool" || all[i].className == "calendarToday") {
+		//Work some magic on certain days (add pictures)
+		var img = document.createElement('img')
+		img.setAttribute("src", chrome.extension.getURL("images/" + all[i].className + ".png"));
+		img.style.width = "95%";
+		img.style.display = "block"; //center
+		img.style.margin = "0 auto"; //center
+		all[i].appendChild(img);
 	}
-	//Kill browser warning, .hw_post, and .hw_remind
-	else if ((all[i].className == 'hw_post') || (all[i].className == 'hw_remind') || all[i].className == 'errorMessage') {
-		all[i].parentNode.removeChild(all[i]);
-	}
+	
 	//Replace all non-crucial occurrences of "The Nueva School" with "Nueva"
-	else if (all[i].innerHTML.match(/(<[^>]*?>[^><]*?)The Nueva School([^><]*?<)/ig)) {
+	if (all[i].innerHTML.match(/(<[^>]*?>[^><]*?)The Nueva School([^><]*?<)/ig)) {
 		all[i].innerHTML = all[i].innerHTML.replace(/(<[^>]*?>[^><]*?)The Nueva School([^><]*?<)/ig, "$1Nueva$2");
 	}
-}
-
-//Work some CSS magic on no-school days
-var noschool = document.getElementsByClassName("calendarNoSchool");
-
-for (var i = 0; i < noschool.length; i++) {
-	var img = document.createElement('img')
-	img.setAttribute("src", "https://fc02.deviantart.net/fs71/f/2012/025/b/b/no_school_by_wilpah-d4nn1c2.png");
-	img.style.width = "95%";
-	img.style.display = "block";
-    img.style.margin = "0 auto";
-	noschool[i].appendChild(img);
 }
