d91862390259792a66e492b5770b65aab3b0e6ca
c0d3rman <yonilern@gmail.com>
2014-01-30 22:10:51 -0800

    2.4 Major Update
    Added background refreshing of Nueva page so it never logs out.
    Also did some tabbing.
    In planning are a button to control the auto-refresh and an alert if it
    fails. Maybe even an option to not refresh/refresh less often based on
    network speed.

--- background.js
+++ background.js
@@ -1,7 +1,26 @@
 function checkForValidUrl(tabId, changeInfo, tab) {
-  if (tab.url.indexOf('my.nuevaschool.org') == 7 || tab.url.indexOf('my.nuevaschool.org') == 8) {
-    chrome.pageAction.show(tabId);
-  }
+	if (tab.url.indexOf('my.nuevaschool.org') == 7 || tab.url.indexOf('my.nuevaschool.org') == 8) {
+		chrome.pageAction.show(tabId);
+	}
 };
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+//var go = true;
+
+function stayLoggedIn(timeout){
+//	if (go) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", 'https://my.nuevaschool.org/', true);
+		/*xhr.addEventListener("load", function(e) {
+			if (xhr.responseText.match(/The Nueva School Home/)) {
+				console.log(xhr.responseText);
+				go = false;
+			}
+		}, false)*/
+		xhr.send();
+		setTimeout(stayLoggedIn, timeout, timeout);
+//	}
+}
+
+stayLoggedIn(90000);
