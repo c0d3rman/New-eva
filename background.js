428f113171b66f25b3d2c32be4c3136224cb46fe
c0d3rman <yonilern@gmail.com>
2014-03-17 18:47:57 -0700

    Backup Before V3
    Backing up changes before V3. In the middle of implementing live
    classlist.

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
+//Get hash of title-text -> class name
+var titlehash = {};
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+		for (line in xhr.responseText.split(/\r\n|\r|\n/)) {
+			var kv = line.split(",");
+			titlehash[kv[0]] = kv[1];
+		}
+		//console.log(titlehash);
+		chrome.storage.local.set({'titlehash': titlehash});
+    }
+}
+
+xhr.open("GET", 'http://txt.do/12ex/raw', true);
+xhr.send();
+
 function checkForValidUrl(tabId, changeInfo, tab) {
 	if (tab.url.indexOf('my.nuevaschool.org') == 7 || tab.url.indexOf('my.nuevaschool.org') == 8) {
 		chrome.pageAction.show(tabId);
@@ -9,9 +26,9 @@ chrome.tabs.onUpdated.addListener(checkForValidUrl);
 chrome.alarms.create("Refresh Nueva Page", {"periodInMinutes": localStorage['refreshPeriod'] || 5});
 
 function stayLoggedIn(){
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", 'https://my.nuevaschool.org/', false);
-	xhr.send();
+	var xhr2 = new XMLHttpRequest();
+	xhr2.open("GET", 'https://my.nuevaschool.org/', false);
+	xhr2.send();
 }
 
 chrome.alarms.onAlarm.addListener(stayLoggedIn);
--- contentscript.js
+++ contentscript.js
@@ -8,168 +8,110 @@ function inject (file) {
 	(document.head||document.documentElement).appendChild(s);
 };
 inject("dynamictag.js"); //inject dynamic script tag
-//inject sum.js and dependencies
-/*inject("lib/sum_js/lib/porter-stemmer.js");
-inject("lib/sum_js/lib/underscore-1.2.3.js");
-inject("lib/sum_js/lib/underscore.string-2.0.0.js");
-inject("lib/sum_js/sum.js");*/
-//Done injecting
 
 //Remove logout timer bar (visual)
 var parent = document.getElementById("__thermo").parentElement;
 parent.parentElement.removeChild(parent);
 
-//Create hash of title-text -> class name
-var titlehash = {
-	//verified
-	'For class "9th CESL".': 'CESL',
-	'For class "9th Comp Sci - Elegant Logic 2013-2014.1".': 'Programming',
-	'For class "9th Comp Sci - Computer Science and Programming 2013-2014.1".': 'Programming',
-	'For class "9th DT Liquid 2013-2014.1".': 'Design Thinking',
-	'For class "9th Service and Design Engineering Plasma 2013-2014.1".': 'Design Engineering',
-	'For class "9th English Baldwin 2013-2014.1".': 'English',
-	'For class "9th English Euripides 2013-2014.1".': 'English',
-	'For class "9th History Rousseau 2013-2014.1".': 'History',
-	'For class "9th History Machiavelli 2013-2014.1".': 'History',
-	'For class "9th Math Khayyam 2013-2014.1".': 'Math',
-	'For class "9th QUEST - All Grade 2013-2014.1".': 'Quest',
-	'For class "9th Quest 2 2013-2014.1".': 'Quest',
-	'For class "9th Robotics 2013-2014.1".': 'Robotics',
-	'For class "9th Robotics FRC Team 2013-2014.1".': 'Robotics',
-	'For class "9th Science and Society 2013-2014.1".': 'Science and Society',
-	'For class "9th Science Liquid 2013-2014.1".': 'Science',
-	'For class "9th Science of Mind Plasma 2013-2014.1".': 'Science of Mind',
-	'For class "9th Science of Mind Liquid 2013-2014.1".': 'Science of Mind',
-	'For class "9th Spanish Inca 2013-2014.1".': 'Spanish',
-	'For class "9th Spanish Azteca 2013-2014.1".': 'Spanish',
-	//educated guesses
-	'For class "9th DT Solid 2013-2014.1".': 'Design Thinking',
-	'For class "9th DT Gas 2013-2014.1".': 'Design Thinking',
-	'For class "9th DT Plasma 2013-2014.1".': 'Design Thinking',
-	'For class "9th Service and Design Engineering Solid 2013-2014.1".': 'Design Engineering',
-	'For class "9th Service and Design Engineering Liquid 2013-2014.1".': 'Design Engineering',
-	'For class "9th Service and Design Engineering Gas 2013-2014.1".': 'Design Engineering',
-	'For class "9th Science Solid 2013-2014.1".': 'Science',
-	'For class "9th Science Gas 2013-2014.1".': 'Science',
-	'For class "9th Science Plasma 2013-2014.1".': 'Science',
-	'For class "9th Science of Mind Solid 2013-2014.1".': 'Science of Mind',
-	'For class "9th Science of Mind Gas 2013-2014.1".': 'Science of Mind',
-	'For class "9th English De La Cruz 2013-2014.1".': 'English',
-	'For class "9th English Kincaid 2013-2014.1".': 'English',
-	'For class "9th English Behn 2013-2014.1".': 'English',
-	'For class "9th History Locke 2013-2014.1".': 'History',
-	'For class "9th History Hobbes 2013-2014.1".': 'History',
-	'For class "9th Math Turing 2013-2014.1".': 'Math',
-	'For class "9th Math Ramanujan 2013-2014.1".': 'Math',
-	'For class "9th Math Noether 2013-2014.1".': 'Math',
-	'For class "9th Comp Sci - Computing in Everything 2013-2014.1".': 'Programming',
-	'For class "9th Spanish Maya 2013-2014.1".': 'Spanish',
+var titlehasher = function (string) {
+	string = string.replace(/For class "(.+) \d{4}-\d{4}\.\d"\./, "$1");
+	//console.log(string + " | " + titlehash[string]);
+	return titlehash[string];
 };
 
-//Function to resolve 2013-2014.1 or 2012-2013.2 issue
-var titlehasher = function (string) {
-	string = string.replace(/\d{4}-\d{4}\.\d/, '2013-2014.1');
-	console.log(string);
-	return titlehash[string];
-}
+//Get hash of title-text -> class name
+var titlehash;
+chrome.storage.local.get('titlehash', function(a){
+	titlehash = a['titlehash'];
+	console.log(titlehash);
 
+	//Function to resolve 2013-2014.1 or 2012-2013.2 issue
 
-//Process all elements (necessary on all pages for "The Nueva School" regex without reloading HTML)
-var all = document.getElementsByTagName("*");
-var max = all.length; //save length to prevent NodeList funkiness
-for (var i = max-1; i > 0; i--) {
-	//Kill browser warning, .hw_post, and .hw_remind
-	if (all[i].className == 'hw_post' || all[i].className == 'hw_remind' || all[i].className == 'errorMessage') {
-		all[i].parentNode.removeChild(all[i]);
-	}
-	//Handle homework assignments
-	else if (all[i].className == 'hw_due') {
-		all[i].removeChild(all[i].firstChild); //Kill "due today"s
-		all[i].firstChild.setAttribute("target", "_blank"); //Make links open in new tab
-		//handle comment
-		var comment = /\s+Comment: (.*)$/.exec(all[i].firstChild.title);
-		all[i].firstChild.title = all[i].firstChild.title.replace(/\s+Comment: .*$/, "");
-		var title = titlehasher(all[i].firstChild.title); //Save title-text for reference (firstChild problems), hash lookup for performance
-		if (comment) {
-			comment = comment[1];
-			all[i].firstChild.title = comment; //Set title-text to comment 
-			all[i].firstChild.innerHTML = '<span style="color: red">' + title + "</span>: " + all[i].firstChild.innerHTML + '<span style="color: green"> Comment: ' + comment + '</span>'; //Add classname & comment
-		} else {
-			all[i].firstChild.title = title; //Set title-text to classname 
-			all[i].firstChild.innerHTML = '<span style="color: red">' + title + "</span>: " + all[i].firstChild.innerHTML; //Add classname
+	//Process all elements (necessary on all pages for "The Nueva School" regex without reloading HTML)
+	var all = document.getElementsByTagName("*");
+	var max = all.length; //save length to prevent NodeList funkiness
+	for (var i = max-1; i > 0; i--) {
+		//Kill browser warning, .hw_post, and .hw_remind
+		if (all[i].className == 'hw_post' || all[i].className == 'hw_remind' || all[i].className == 'errorMessage') {
+			all[i].parentNode.removeChild(all[i]);
 		}
-		all[i].style.listStyleType = 'none'; //Remove bullet points
-	} else if (document.title == "Calendar" && ((all[i].className == "calendarDay" && (all[i] == all[i].parentNode.firstChild || all[i] == all[i].parentNode.lastChild)) || all[i].className == "calendarNoSchool" || all[i].className == "calendarToday")) {
-		// || (all[i].className == "calendarDay" && (all[i] == all[i].parentNode.firstChild || all[i] == all[i].parentNode.lastChild))
-		var filename = all[i].className; //save filename for special case handling
+		//Handle homework assignments
+		else if (all[i].className == 'hw_due') {
+			all[i].removeChild(all[i].firstChild); //Kill "due today"s
+			all[i].firstChild.setAttribute("target", "_blank"); //Make links open in new tab
+				//handle comment
+				var comment = /\s+Comment: (.*)$/.exec(all[i].firstChild.title);
+				all[i].firstChild.title = all[i].firstChild.title.replace(/\s+Comment: .*$/, "");
+				var title = titlehasher(all[i].firstChild.title); //Save title-text for reference (firstChild problems), hash lookup for performance
+				if (comment) {
+					comment = comment[1];
+					all[i].firstChild.title = comment; //Set title-text to comment 
+					all[i].firstChild.innerHTML = '<span style="color: red">' + title + "</span>: " + all[i].firstChild.innerHTML + '<span style="color: green"> Comment: ' + comment + '</span>'; //Add classname & comment
+				} else {
+					all[i].firstChild.title = title; //Set title-text to classname 
+					all[i].firstChild.innerHTML = '<span style="color: red">' + title + "</span>: " + all[i].firstChild.innerHTML; //Add classname
+				}
+				all[i].style.listStyleType = 'none'; //Remove bullet points
+			} else if (document.title == "Calendar" && ((all[i].className == "calendarDay" && (all[i] == all[i].parentNode.firstChild || all[i] == all[i].parentNode.lastChild)) || all[i].className == "calendarNoSchool" || all[i].className == "calendarToday")) {
+				// || (all[i].className == "calendarDay" && (all[i] == all[i].parentNode.firstChild || all[i] == all[i].parentNode.lastChild))
+				var filename = all[i].className; //save filename for special case handling
 		
-		//handle sundays and saturdays
-		//if ((localStorage["weekend"] == "0" && all[i].className == "calendarToday") || (localStorage["weekend"] == "1" && all[i].className == "calendarDay")) {
-		if (all[i].className == "calendarDay") {
-			if (all[i] == all[i].parentNode.firstChild) {
-				filename = "calendarSunday";
-			} else {
-				filename = "calendarSaturday";
+				//handle sundays and saturdays
+				//if ((localStorage["weekend"] == "0" && all[i].className == "calendarToday") || (localStorage["weekend"] == "1" && all[i].className == "calendarDay")) {
+				if (all[i].className == "calendarDay") {
+					if (all[i] == all[i].parentNode.firstChild) {
+						filename = "calendarSunday";
+					} else {
+						filename = "calendarSaturday";
+					}
+				}
+		
+				//Work some magic on certain days (add pictures)
+				var img = document.createElement('img')
+				img.setAttribute("src", chrome.extension.getURL("images/" + filename + ".png"));
+				img.style.width = "95%";
+				img.style.display = "block"; //center
+				img.style.margin = "0 auto"; //center
+				all[i].appendChild(img);
+			}
+	
+			//Replace all non-crucial occurrences of "The Nueva School" with "Nueva"
+			if (all[i].innerHTML.match(/(<[^>]*?>[^><]*?)The Nueva School([^><]*?<)/ig)) {
+				all[i].innerHTML = all[i].innerHTML.replace(/(<[^>]*?>[^><]*?)The Nueva School([^><]*?<)/ig, "$1Nueva$2");
 			}
 		}
-		
-		//Work some magic on certain days (add pictures)
-		var img = document.createElement('img')
-		img.setAttribute("src", chrome.extension.getURL("images/" + filename + ".png"));
-		img.style.width = "95%";
-		img.style.display = "block"; //center
-		img.style.margin = "0 auto"; //center
-		all[i].appendChild(img);
-	}
-	
-	//Replace all non-crucial occurrences of "The Nueva School" with "Nueva"
-	if (all[i].innerHTML.match(/(<[^>]*?>[^><]*?)The Nueva School([^><]*?<)/ig)) {
-		all[i].innerHTML = all[i].innerHTML.replace(/(<[^>]*?>[^><]*?)The Nueva School([^><]*?<)/ig, "$1Nueva$2");
-	}
-}
 
-//Add footer text
-document.getElementById('footer').firstChild.style.width = 'auto';
-document.getElementById('footer').firstChild.innerHTML += ' &nbsp; &nbsp;New-eva extension by Yoni Lerner';
+		//Add footer text
+		document.getElementById('footer').firstChild.style.width = 'auto';
+		document.getElementById('footer').firstChild.innerHTML += ' &nbsp; &nbsp;New-eva extension by Yoni Lerner';
 
-//Check if current tab is homework calendar
-/*if (document.getElementsByClassName('subnavTitle')[0].innerText == " Homework Calendar     Print") {
-	localStorage["homework"] = document.getElementsByClassName('calendarToday')[0].nextSibling.innerHTML; //get tomorrow's assignments
-	localStorage["date"] = Date.now();
-}*/
+		//Check if current tab is homework calendar
+		/*if (document.getElementsByClassName('subnavTitle')[0].innerText == " Homework Calendar     Print") {
+			localStorage["homework"] = document.getElementsByClassName('calendarToday')[0].nextSibling.innerHTML; //get tomorrow's assignments
+			localStorage["date"] = Date.now();
+		}*/
 
-//create sidebox with hw for tomorrow
-var sidebox = document.createElement('div');
-var c = document.createElement('div');
-//sidebox.innerHTML = "<span style='font: 10px gray'>" + Math.floor((Date.now() - localStorage['date']) / 60000) + " minutes since last updated</span>"; //Display time since last updated
-//sidebox.innerHTML += localStorage["homework"]; //get homework from memory
-sidebox.innerHTML = document.getElementsByClassName('calendarToday')[0].nextSibling.innerHTML;
-sidebox.firstChild.firstChild.innerText = "Tomorrow's Homework (" + sidebox.firstChild.firstChild.innerText + ")"; //set title
-//style sidebox
-sidebox.style.position = "fixed";
-sidebox.style.padding = "10 5";
-sidebox.style.zIndex = 1000;
-sidebox.style.width = 100;
-sidebox.style.marginTop = 100;
-sidebox.style.marginRight = "10px";
-//sidebox.style.float = "right";
-sidebox.style.marginLeft = (window.innerWidth - 126);
-sidebox.style.border = "3px solid black";
-sidebox.style.backgroundColor = "#CCCCCC";
-document.body.insertBefore(sidebox, document.body.firstChild); //add to beginning of page
+		//create sidebox with hw for tomorrow
+		var sidebox = document.createElement('div');
+		var c = document.createElement('div');
+		//sidebox.innerHTML = "<span style='font: 10px gray'>" + Math.floor((Date.now() - localStorage['date']) / 60000) + " minutes since last updated</span>"; //Display time since last updated
+		//sidebox.innerHTML += localStorage["homework"]; //get homework from memory
+		sidebox.innerHTML = document.getElementsByClassName('calendarToday')[0].nextSibling.innerHTML;
+		sidebox.firstChild.firstChild.innerText = "Tomorrow's Homework (" + sidebox.firstChild.firstChild.innerText + ")"; //set title
+		//style sidebox
+		sidebox.style.position = "fixed";
+		sidebox.style.padding = "10 5";
+		sidebox.style.zIndex = 1000;
+		sidebox.style.width = 100;
+		sidebox.style.marginTop = 100;
+		sidebox.style.marginRight = "10px";
+		sidebox.style.marginLeft = (window.innerWidth - 126);
+		sidebox.style.border = "3px solid black";
+		sidebox.style.backgroundColor = "#CCCCCC";
+		document.body.insertBefore(sidebox, document.body.firstChild); //add to beginning of page
 
-//Change month names to images
-/*var monthnames = document.getElementsByTagName('font a');
-for (var i = 0; i < monthnames.length; i++) {
-	var img = document.createElement('img');
-	img.setAttribute("src", chrome.extension.getURL("images/" + monthnames.innerHTML + ".png"));
-	monthnames.innerHTML = '';
-	img.style.height = "40px";
-//	img.style.display = "block"; //center
-	monthnames[i].appendChild(img);
-}*/
-
-//set onResize to keep sidebox on right side
-window.onresize = function(){
-	sidebox.style.marginLeft = (window.innerWidth - 126);
-};
+		//set onResize to keep sidebox on right side
+		window.onresize = function(){
+			sidebox.style.marginLeft = (window.innerWidth - 126);
+		};
+});
